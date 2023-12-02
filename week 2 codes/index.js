c759ac0420b2e80f4ac1ae187f8bb3227c52cf3d
vwakesahu <viveksahu1762@gmail.com>
2023-12-02 19:29:35 +0530

    Added http handlers get & put

--- week 2 codes/index.js
+++ week 2 codes/index.js
@@ -2,17 +2,19 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-function calSum(n) {
+function calSum(startIndex, lastIndex) {
   var sum = 0;
-  for (let index = 0; index < n; index++) {
+  for (let index = startIndex; index <= lastIndex; index++) {
     sum += index;
   }
   return sum;
 }
 
 function handleDontKnowRequest(req, res) {
-  var n = req.query.n;
-  const calculatedSum = calSum(n);
+  //passing variable using query
+  var lastIndex = parseInt(req.query.n2);
+  var startIndex = parseInt(req.query.n1);
+  const calculatedSum = calSum(startIndex, lastIndex);
   console.log(calculatedSum);
   res.send(`Answer ${calculatedSum}`);
 }
@@ -20,8 +22,13 @@ function handleFirstRequest(req, res) {
   res.send("Hello World");
 }
 
+
+//Browsers URL bar sends only get request http://localhost:3000/dontknow
 app.get("/", handleFirstRequest);
 app.get("/dontknow", handleDontKnowRequest);
+
+//POST request can be sent by using postman client: http://localhost:3000/postRequest
+app.post("/postRequest", handleFirstRequest);
 
 function started() {
   console.log(`Example app listening on port ${port}`);
